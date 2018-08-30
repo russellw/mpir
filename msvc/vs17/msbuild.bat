0abed07f6c5b67b44500360422e2f0c1e91f112e
Brian Gladman <brg@gladman.plus.com>
2018-08-30 19:19:57 +0100

    remove files that should not be in the repository; update msbuild.bat to input the Windows SDK wanted

--- msvc/vs17/msbuild.bat
+++ msvc/vs17/msbuild.bat
@@ -3,7 +3,8 @@ rem %1 = architecture
 rem %2 = library type (LIB|DLL)
 rem %3 = platform (Win32|x64)
 rem %4 = configuration (Release|Debug)
-rem %5 = build tests (|+tests)
+rem %5 = Windows SDK Version
+rem %6 = build tests (|+tests)
 
 set vsw_exe="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
@@ -28,6 +29,7 @@ if not exist "lib_mpir_%1" (call :get_architectures & call :seterr & echo ERROR:
 if /i "%2" EQU "DLL" (set libp=dll) else (if /i "%2" EQU "LIB" (set libp=lib) else ((call :seterr & echo ERROR: library type is "lib" or "dll" ^(not "%2"^) & exit /b %errorlevel%)))
 if /i "%3" EQU "x64" (set plat=x64) else (if /i "%3" EQU "Win32" (set plat=win32) else (call :seterr & echo ERROR: platform is "Win32" or "x64" ^(not "%3"^) & exit /b %errorlevel%))
 if /i "%4" EQU "Debug" (set conf=Debug) else (if /i "%4" EQU "Release" (set conf=Release) else (call :seterr & echo ERROR: configuration is "Release" or "Debug" ^(not "%4"^) & exit /b %errorlevel%))
+if "%5" NEQ "" (set win_sdk="%5")
 
 set src=%libp%_mpir_%1
 
@@ -35,16 +37,16 @@ rem This is the Visual Studio build directory (within the MPIR directory)
 set srcdir="."
 
 echo Using MSBuild at: %msbdir%
-%msbdir%\msbuild.exe /p:Platform=%plat% /p:Configuration=%conf% %srcdir%\%src%\%src%.vcxproj
+%msbdir%\msbuild.exe /p:Platform=%plat% /p:Configuration=%conf% /p:"Windows%20SDK%20Version=%win_sdk%" %srcdir%\%src%\%src%.vcxproj
 
 if /i "%libp%" == "LIB" (
-  %msbdir%\msbuild.exe /p:Platform=%plat% /p:Configuration=%conf% %srcdir%\lib_mpir_cxx\lib_mpir_cxx.vcxproj
+  %msbdir%\msbuild.exe /p:Platform=%plat% /p:Configuration=%conf% /p:"Windows%20SDK%20Version=%win_sdk%" %srcdir%\lib_mpir_cxx\lib_mpir_cxx.vcxproj
 )
 
-if /i "%5" EQU "+tests" (
+if /i "%6" EQU "+tests" (
   for /d %%d in (.\mpir-tests\*) do (
     for %%f in (%%d\*.vcxproj) do (
-      %msbdir%\msbuild.exe /property:SolutionDir=..\..\ /property:OutDir=..\..\%plat%\%conf%\ /p:Platform=%plat% /p:Configuration=%conf% %%f
+      %msbdir%\msbuild.exe /property:SolutionDir=..\..\ /property:OutDir=..\..\%plat%\%conf%\ /p:Platform=%plat% /p:Configuration=%conf% /p:"Windows%20SDK%20Version=%win_sdk%" %%f
     )
   )
 )
