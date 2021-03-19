cef0f481f5ea1a41f691d295b93b432d9e13ee1f
Russell Wallace <russell.wallace@gmail.com>
2021-03-19 01:49:14 +0000

    build

--- build.bat
+++ build.bat
@@ -2,6 +2,10 @@ md obj
 cl /Foobj\ /I. /c *.c
 if errorlevel 1 goto :eof
 
+md obj\fft
+cl /Foobj\fft\ /I. /c fft\*.c
+if errorlevel 1 goto :eof
+
 md obj\mpn
 cl /Foobj\mpn\ /I. /c mpn\*.c
 if errorlevel 1 goto :eof
@@ -10,7 +14,7 @@ md obj\mpz
 cl /Foobj\mpz\ /I. /c mpz\*.c
 if errorlevel 1 goto :eof
 
-lib /out:mpir.lib obj\*.obj obj\mpn\*.obj obj\mpz\*.obj
+lib /out:mpir.lib obj\*.obj obj\fft\*.obj obj\mpn\*.obj obj\mpz\*.obj
 if errorlevel 1 goto :eof
 
 cl /I. test.c mpir.lib
