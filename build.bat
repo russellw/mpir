17e9e2dc7a7684e4d939da547e2c96e42c566610
Russell Wallace <russell.wallace@gmail.com>
2021-03-20 14:47:40 +0000

    assert

--- build.bat
+++ build.bat
@@ -5,35 +5,35 @@ rem debug build
 
 rd /s /q obj
 md obj
-cl /DDEBUG /Foobj\ /I. /MP4 /MTd /WX /c *.c
+cl /DDEBUG /DWANT_ASSERT=1 /Foobj\ /I. /MP4 /MTd /WX /c *.c
 if errorlevel 1 goto :eof
 
 md obj\fft
-cl /Foobj\fft\ /I. /MP4 /MTd /WX /c fft\*.c
+cl /DWANT_ASSERT=1 /Foobj\fft\ /I. /MP4 /MTd /WX /c fft\*.c
 if errorlevel 1 goto :eof
 
 md obj\mpf
-cl /Foobj\mpf\ /I. /MP4 /MTd /WX /c mpf\*.c
+cl /DWANT_ASSERT=1 /Foobj\mpf\ /I. /MP4 /MTd /WX /c mpf\*.c
 if errorlevel 1 goto :eof
 
 md obj\mpn
-cl /Foobj\mpn\ /I. /MP4 /MTd /WX /c mpn\*.c
+cl /DWANT_ASSERT=1 /Foobj\mpn\ /I. /MP4 /MTd /WX /c mpn\*.c
 if errorlevel 1 goto :eof
 
 md obj\mpq
-cl /Foobj\mpq\ /I. /MP4 /MTd /WX /c mpq\*.c
+cl /DWANT_ASSERT=1 /Foobj\mpq\ /I. /MP4 /MTd /WX /c mpq\*.c
 if errorlevel 1 goto :eof
 
 md obj\mpz
-cl /Foobj\mpz\ /I. /MP4 /MTd /WX /c mpz\*.c
+cl /DWANT_ASSERT=1 /Foobj\mpz\ /I. /MP4 /MTd /WX /c mpz\*.c
 if errorlevel 1 goto :eof
 
 md obj\printf
-cl /Foobj\printf\ /I. /MP4 /MTd /WX /c printf\*.c
+cl /DWANT_ASSERT=1 /Foobj\printf\ /I. /MP4 /MTd /WX /c printf\*.c
 if errorlevel 1 goto :eof
 
 md obj\scanf
-cl /Foobj\scanf\ /I. /MP4 /MTd /WX /c scanf\*.c
+cl /DWANT_ASSERT=1 /Foobj\scanf\ /I. /MP4 /MTd /WX /c scanf\*.c
 if errorlevel 1 goto :eof
 
 lib /out:debug.lib obj\*.obj obj\fft\*.obj obj\mpf\*.obj obj\mpn\*.obj obj\mpq\*.obj obj\mpz\*.obj obj\printf\*.obj obj\scanf\*.obj
@@ -95,7 +95,7 @@ for /r %%x in (t-*.c) do (
 	rem disable warning about printf %l specifier for 64-bit integers
 	rem the code is full of these, which were valid on UNIX
 	rem while not valid on Windows, hopefully they will do no harm
-	cl /Fea /I. /Itests /MTd /WX /wd4477 %%x tests/memory.c tests/misc.c tests/ref*.c tests/trace.c debug.lib
+	cl /DWANT_ASSERT=1 /Fea /I. /Itests /MTd /WX /wd4477 %%x tests/memory.c tests/misc.c tests/ref*.c tests/trace.c debug.lib
 	if errorlevel 1 goto :eof
 	a
 	if errorlevel 1 goto :eof
