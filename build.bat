e4e0c6d1147380bb275ca528986c9d5a6af12886
Russell Wallace <russell.wallace@gmail.com>
2023-04-16 10:49:29 +0100

    VS 2022

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+if "%VCINSTALLDIR%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 rem debug build
 
